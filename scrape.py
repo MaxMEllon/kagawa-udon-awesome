b89c72f500ed7d4b82c4f594aab661979416ceb4
MaxMEllon <maxmellon1994@gmail.com>
2017-06-24 17:16:35 +0900

    Refactoring

--- scrape.py
+++ scrape.py
@@ -4,16 +4,18 @@ from bs4 import BeautifulSoup as bs
 
 base_url = 'https://tabelog.com/udon/kagawa/rank/'
 
+def sequence(url):
+    res = requests.get(url)
+    content = res.content
+    soup = bs(content, 'html.parser')
+    shops = soup.select('div#main-contents > ul > li > div > h3 > strong > a')
+    links = list(map(lambda s: { 'link': s['href'] }, shops))
+    with open('list.json', 'w', encoding='utf-8') as fp:
+        json.dump({ 'all': links  }, fp)
+    [print('| [' + shop.text + '](' + shop['href'] + ') | | | | | | | | |') for shop in shops]
+
 def main():
-    for i in map(lambda i: i + 1, range(5)):
-        res = requests.get(base_url + str(i) + '/')
-        content = res.content
-        soup = bs(content, 'html.parser')
-        shops = soup.select('div#main-contents > ul > li > div > h3 > strong > a')
-        links = list(map(lambda s: { 'link': s['href'] }, shops))
-        with open('list.json', 'w', encoding='utf-8') as fp:
-            json.dump({ 'all': links  }, fp)
-        [print('| [' + shop.text + '](' + shop['href'] + ') | | | | | | | | |') for shop in shops]
+    [sequence(base_url + str(i) + '/') for i in map(lambda i: i + 1, range(5))]
 
 if __name__ == '__main__':
     main()
